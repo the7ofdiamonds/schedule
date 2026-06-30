6845fa5a9e4004815ced20d601965b36eb74cd95
jamelclyons <jamel.c.lyons@gmail.com>
2026-06-30 15:08:18 -0400

    Refactor on 2026-06-30 @ 15:08:18

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite';
 
 import dts from 'vite-plugin-dts';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 import path from 'path';
-
-import rollupOptions from './rollup.config';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [
     dts({ insertTypesEntry: true, outDir: 'dist/types' }),
-    tsconfigPaths(),
   ],
   build: {
     cssCodeSplit: true,
@@ -21,9 +17,8 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'Schedule',
       formats: ['es', 'cjs'],
-      fileName: (format, name) => `${name}.${format === 'es' ? 'js' : format}`,
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
     },
-    rollupOptions: rollupOptions,
     minify: false,
   },
   resolve: {
